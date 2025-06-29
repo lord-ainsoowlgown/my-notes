c8489720d7bb8c68ac7022260f10d2f02bc2b6e6
lord-ainsoowlgown <ainsoowlgown0@gmail.com>
2025-06-29 23:40:18 +0530

    Optimised Bloc

--- lib/helpers/loading/loading_screen.dart
+++ lib/helpers/loading/loading_screen.dart
@@ -25,7 +25,7 @@ class LoadingScreen {
   }
 
   void hide() {
-    controller!.close();
+    controller?.close();
     controller = null;
   }
 
@@ -46,47 +46,47 @@ class LoadingScreen {
           color: Colors.black.withAlpha(150),
           child: Center(
             child: Container(
-              constraints: BoxConstraints(
-                maxWidth: size.width * 0.8,
-                maxHeight: size.height * 0.8,
-                minWidth: size.height * 0.5,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 10),
-                      const CircularProgressIndicator(
-                        color: Colors.blue,
-                      ),
-                      const SizedBox(height: 20),
-                      StreamBuilder(stream: _text.stream, builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          return Text(
-                            snapshot.data as String,
-                            textAlign: TextAlign.center,
-                          );
-                        } else {
-                          return Container();
-                        }
-                      }),
-                    ],
-                  ),
+                constraints: BoxConstraints(
+                  maxWidth: size.width * 0.8,
+                  maxHeight: size.height * 0.8,
+                  minWidth: size.width * 0.5,
                 ),
-              ),
-            ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 10),
+                        const CircularProgressIndicator(),
+                        const SizedBox(height: 20),
+                        StreamBuilder(
+                          stream: _text.stream,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Text(
+                                snapshot.data as String,
+                                textAlign: TextAlign.center,
+                              );
+                            } else {
+                              return Container();
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
           ),
         );
       },
     );
- 
+
     state?.insert(overlay);
 
     return LoadingScreenController(
@@ -95,7 +95,7 @@ class LoadingScreen {
         overlay.remove();
         return true;
       },
-      update: (String text) {
+      update: (text) {
         _text.add(text);
         return true;
       },
